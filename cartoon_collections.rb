9a809f151086e4d7f98203ef3970942371dccce9
Rashad Dixon <dixoncodes@gmail.com>
2019-12-26 23:16:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(source_array)
   new_array = source_array.each_with_index{|dwarf, index|}
   i = 0
   while i < new_array.length
-  puts "#{i}.*#{new_array[i]}"
+  puts "#{i + 1}.*#{new_array[i]}"
   i += 1
   end
   return new_array
