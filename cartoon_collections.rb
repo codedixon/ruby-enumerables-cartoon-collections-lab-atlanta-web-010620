960ababeca5dc1934998a4fbbccb1638257e9226
Rashad Dixon <dixoncodes@gmail.com>
2019-12-26 23:45:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ end
 def summon_captain_planet(source_array)
   new_array = source_array.capitalize()
 new_array.map{|x| x + "!"}
+return new_array
 end
 
 def long_planeteer_calls# code an argument here
