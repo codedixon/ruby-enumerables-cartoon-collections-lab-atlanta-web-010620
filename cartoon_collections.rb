be677cdd352b35ffb3a6fbd8fd2c9c7e89c3b0ea
Rashad Dixon <dixoncodes@gmail.com>
2019-12-27 01:31:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,4 +22,5 @@ def find_the_cheese(source_array)
   # the array below is here to help
   find_cheese = source_array.find {|x| x == "cheddar" || x == "gouda" || x == "camembert"}
   cheese_types = ["cheddar", "gouda", "camembert"]
+  return find_cheese
 end
