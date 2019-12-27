bfc9e03819e2ed09827da52b0655341d16701b46
Rashad Dixon <dixoncodes@gmail.com>
2019-12-27 01:26:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ letters = source_array.map {|x| x.length}
 value = letters.any? {|x| x > 4} 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(source_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
